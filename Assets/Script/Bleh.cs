ef1780d67ee439428ed6a8a62d0f01fe8c3147ae
Nia Geary-Andrews <n.e.geary-andrews@bham.ac.uk>
2024-11-11 13:17:40 +0000

    Testing some Button Code

--- Assets/Script/Bleh.cs
+++ Assets/Script/Bleh.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class Bleh
+public class Bleh:MonoBehaviour
 {
     public Button Button1;
-    public Button Button2;
+    public TextMeshProUGUI Text;
+
+    public void OnButtonPress()
+    {
+        Text.text = "Clicked";
+    }
 }
+

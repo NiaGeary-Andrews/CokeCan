edf5f21233b2c96e2c839181188f9c695ef71c7a
Nia Geary-Andrews <n.e.geary-andrews@bham.ac.uk>
2024-11-29 10:25:13 +0000

    Added in locking menu and shuffled round challenge order

--- Assets/Script/GuessChecker.cs
+++ Assets/Script/GuessChecker.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GuessChecker : MonoBehaviour
@@ -21,6 +22,14 @@ public class GuessChecker : MonoBehaviour
             outputText.text = "Correct! The number you need is 8!";
             guess.interactable = false;
             submitButton.interactable = false;
+
+            if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+            {
+                Debug.Log("Here");
+                PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+                PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+                PlayerPrefs.Save();
+            }
         }
         else
         {
